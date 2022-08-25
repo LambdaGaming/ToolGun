ac15983a0acd8be35474a323b0ae911c371eff8c
LambdaGaming <starwars18m@gmail.com>
2022-08-25 15:51:51 -0400

    Finish serial conversion

--- ir_transmitter/ir_transmitter.ino
+++ ir_transmitter/ir_transmitter.ino
@@ -5,7 +5,7 @@
 IRsend sender;
 uint16_t CurrentAddress = 0;
 uint16_t CurrentCommand = 0;
-decode_type_t CurrentProtocol = UNKNOWN;
+String CurrentProtocol = "";
 String input[2];
 
 void parseInput( String str )
@@ -16,60 +16,50 @@ void parseInput( String str )
 	input[1] = str.substring( index + 1, index2 );
 }
 
-decode_type_t getProtocolFromString( String p )
-{
-	p.toLowerCase();
-	if ( p == "nec" ) {
-		CurrentProtocol = NEC;
-	} else if ( p == "samsung" ) {
-		CurrentProtocol = SAMSUNG;
-	} else if ( p == "samsung_lg" ) {
-		CurrentProtocol = SAMSUNG_LG;
-	} else if ( p == "sony" ) {
-		CurrentProtocol = SONY;
-	} else if ( p == "panasonic" ) {
-		CurrentProtocol = PANASONIC;
-	} else if ( p == "denon" ) {
-		CurrentProtocol = DENON;
-	} else if ( p == "sharp" ) {
-		CurrentProtocol = SHARP;
-	} else if ( p == "lg" ) {
-		CurrentProtocol = LG;
-	} else if ( p == "jvc" ) {
-		CurrentProtocol = JVC;
-	} else if ( p == "rc5" ) {
-		CurrentProtocol = RC5;
-	} else if ( p == "rc6" ) {
-		CurrentProtocol = RC6;
-	} else if ( p == "kaseikyo_jvc" ) {
-		CurrentProtocol = KASEIKYO_JVC;
-	} else if ( p == "kaseikyo_denon" ) {
-		CurrentProtocol = KASEIKYO_DENON;
-	} else if ( p == "kaseikyo_sharp" ) {
-		CurrentProtocol = KASEIKYO_SHARP;
-	} else if ( p == "kaseikyo_mitsubishi" ) {
-		CurrentProtocol = KASEIKYO_MITSUBISHI;
-	} else if ( p == "onkyo" ) {
-		CurrentProtocol = ONKYO;
-	} else if ( p == "apple" ) {
-		CurrentProtocol = APPLE;
-#ifndef EXCLUDE_EXOTIC_PROTOCOLS
-	} else if ( p == "bosewave" ) {
-		CurrentProtocol = BOSEWAVE;
-	} else if ( p == "lego_pf" ) {
-		CurrentProtocol = LEGO_PF;
-#endif
-	}
-}
-
 void sendIR()
 {
-	IRData data;
-	data.address = CurrentAddress;
-	data.command = CurrentCommand;
-	data.protocol = CurrentProtocol;
-	sender.write( &data, 0 );
-	Serial.println( "test" );
+	CurrentProtocol.toLowerCase();
+	if ( CurrentProtocol == "nec" ) {
+  	sender.sendNEC( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "samsung" ) {
+		sender.sendSamsung( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "samsung_lg" ) {
+		sender.sendSamsungLG( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "sony" ) {
+		sender.sendSony( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "panasonic" ) {
+		sender.sendPanasonic( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "denon" ) {
+		sender.sendDenon( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "sharp" ) {
+		sender.sendSharp( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "lg" ) {
+		sender.sendLG( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "jvc" ) {
+		sender.sendJVC( ( uint8_t ) CurrentAddress, ( uint8_t ) CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "rc5" ) {
+		sender.sendRC5( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "rc6" ) {
+		sender.sendRC6( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "kaseikyo_jvc" ) {
+		sender.sendKaseikyo_JVC( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "kaseikyo_denon" ) {
+		sender.sendKaseikyo_Denon( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "kaseikyo_sharp" ) {
+		sender.sendKaseikyo_Sharp( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "kaseikyo_mitsubishi" ) {
+		sender.sendKaseikyo_Mitsubishi( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "onkyo" ) {
+		sender.sendOnkyo( CurrentAddress, CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "apple" ) {
+		sender.sendApple( CurrentAddress, CurrentCommand, 0 );
+#ifndef EXCLUDE_EXOTIC_PROTOCOLS
+	} else if ( CurrentProtocol == "bosewave" ) {
+		sender.sendBoseWave( CurrentCommand, 0 );
+	} else if ( CurrentProtocol == "lego_pf" ) {
+		sender.sendLegoPowerFunctions( CurrentAddress, CurrentCommand, CurrentCommand >> 4 );
+#endif
+	}
 }
 
 void setup()
@@ -86,9 +76,9 @@ void loop()
 		parseInput( Serial.readString() );
 		if ( input[0] == "fire" )
 		{
-	  		digitalWrite( LED_BUILTIN, HIGH );
-			sendIR();
-			digitalWrite( LED_BUILTIN, LOW );
+	    digitalWrite( LED_BUILTIN, HIGH );
+		  sendIR();
+		  digitalWrite( LED_BUILTIN, LOW );
 		}
 		else if ( input[0] == "address" )
 		{
@@ -100,7 +90,7 @@ void loop()
 		}
 		else if ( input[0] == "protocol" )
 		{
-			CurrentProtocol = getProtocolFromString( input[1] );
+			CurrentProtocol = input[1];
 		}
 	}
 }

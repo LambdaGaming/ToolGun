d23218e3f68020592923105870e7bbdaf39ec320
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-09-05 23:50:57 -0400

    Update how requirements are installed

---
+++ install_requirements.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-pyqt5 python3-pyqt5.qtwebengine python3-pyqt5.qtwebchannel libqt5webkit5-dev python3-gpiozero python3-psutil python3-pynput python3-requests python3-webview

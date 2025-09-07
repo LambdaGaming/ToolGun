4674a3661f0e8df8052a90aef3d7a75c75ace43f
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-09-06 23:40:01 -0400

    Remove pynput requirement

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.1 python3-gpiozero python3-psutil python3-pynput python3-requests
+sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.1 python3-gpiozero python3-psutil python3-requests
 pip install pywebview[gtk] --break-system-packages
